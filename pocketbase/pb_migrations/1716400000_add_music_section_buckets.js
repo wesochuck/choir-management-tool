237333c4c052f6d61d2a63e034759cc739855b1f
Wes Osborn <wosborn@clcohio.org>
2026-05-21 14:20:41 -0400

    feat(db): add sectionBuckets field to musicLibrary collection

---
+++ pocketbase/pb_migrations/1716400000_add_music_section_buckets.js
@@ -0,0 +1,19 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_music_library_001");
+  
+  collection.fields.push(new JsonField({
+    name: "sectionBuckets",
+    required: false,
+    presentable: false
+  }));
+
+  app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_music_library_001");
+  const field = collection.fields.getByName("sectionBuckets");
+  if (field) {
+    collection.fields.removeById(field.id);
+  }
+  app.save(collection);
+});
