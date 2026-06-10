44265b80075942bf93905cc4a71cc34d8aca149b
Wes Osborn <wosborn@clcohio.org>
2026-06-09 22:53:41 -0400

    test: update mobile breakpoint assertions in attendance and check-in list CSS tests

--- test/checkInListCss.test.ts
+++ test/checkInListCss.test.ts
@@ -5,7 +5,7 @@ import { readFileSync } from 'node:fs';
 const css = readFileSync(new URL('../src/components/admin/CheckInList.css', import.meta.url), 'utf8');
 
 test('check-in list mobile CSS hooks exist', () => {
-  assert.match(css, /@media\s*\(max-width:\s*700px\)/, 'mobile breakpoint should exist');
+  assert.match(css, /@media\s*\(width\s*<=\s*700px\)/, 'mobile breakpoint should exist');
   assert.match(css, /\.admin-checkin-mobile-more\s*\{/, 'mobile more button class should exist');
   assert.match(css, /\.admin-checkin-row\.secondary-open\s+\.admin-checkin-bottom-row\s*\{/, 'secondary open rule should exist');
 });
