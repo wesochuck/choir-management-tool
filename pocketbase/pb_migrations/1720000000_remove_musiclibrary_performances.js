5c9cb1a0d37ec95a0733fcb1d4cf3ef9489c2ebe
Wes Osborn <wosborn@clcohio.org>
2026-06-20 22:32:14 -0400

    feat: remove musicLibrary.performances relation field, setList is now single source of truth

---
+++ pocketbase/pb_migrations/1720000000_remove_musiclibrary_performances.js
@@ -0,0 +1,27 @@
+/// <reference path="../pb_data/types.d.ts" />
+
+migrate(
+  (app) => {
+    const collection = app.dao().findCollectionByNameOrId('musicLibrary');
+    const field = collection.fields.find((f) => f.name === 'performances');
+    if (field) {
+      collection.fields.remove(field);
+      app.dao().saveCollection(collection);
+    }
+  },
+  (app) => {
+    // Revert: add the field back
+    const collection = app.dao().findCollectionByNameOrId('musicLibrary');
+    collection.fields.add(
+      new RelationField({
+        name: 'performances',
+        collectionId: 'pbc_1687431684',
+        maxSelect: 999,
+        minSelect: 0,
+        required: false,
+        cascadeDelete: false,
+      })
+    );
+    app.dao().saveCollection(collection);
+  }
+);
