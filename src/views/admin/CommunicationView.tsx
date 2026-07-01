7e7e453d0b02f5b4eb031fa2cf4788142103369b
Wes Osborn <wosborn@clcohio.org>
2026-07-01 13:20:09 -0400

    feat: import useQuery from tanstack/react-query in CommunicationView

--- src/views/admin/CommunicationView.tsx
+++ src/views/admin/CommunicationView.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { AppCard } from '../../components/common/AppCard';
 import EasyMDE from 'easymde';
 import { useDialog } from '../../contexts/DialogContext';
