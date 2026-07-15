ac3b2f775174a3870adf35dae8fe3829a85dd815
Wes Osborn <wosborn@clcohio.org>
2026-07-15 11:50:58 -0400

    refactor: remove unused DeliveryCounts and DeliveryFailureDetail interfaces from communicationService

--- src/services/communicationService.ts
+++ src/services/communicationService.ts
@@ -40,8 +40,6 @@ export type {
   SentTaskStatusOptions,
   DeliveryState,
   FailureCategory,
-  DeliveryCounts,
-  DeliveryFailureDetail,
   DeliverySummary,
   DeliverySummaryResponse,
   RetryFailedResponse,
