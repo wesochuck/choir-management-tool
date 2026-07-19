2bc9f816de284053ac486b10b19543016459460b
wesochuck <cwosborn@gmail.com>
2026-07-19 16:00:50 -0400

    chore: remove temporary HTML stripping diagnostic

--- test_double_escape.js
+++
@@ -1,6 +0,0 @@
-import { JSDOM } from 'jsdom';
-import DOMPurify from 'dompurify';
-const window = new JSDOM('').window;
-const purify = DOMPurify(window);
-const sanitized = purify.sanitize('AT&amp;T', { ALLOWED_TAGS: [], RETURN_DOM: true });
-console.log(sanitized.textContent);
